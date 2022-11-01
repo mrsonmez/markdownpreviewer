8d34be572ae24aea0fdf909f12987920e90075f8
soner_sonmez <ichbinsirpak@gmail.com>
2022-11-01 15:11:22 +0300

    Color adjusted and button added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installation
+
+```
+git clone https://github.com/mrsonmez/markdownpreviewer.git
+```
+
+## Used technologies and libraries
+
+Basic React, react-markdown,basic css
--- src/Components/Previewer.jsx
+++ src/Components/Previewer.jsx
@@ -5,26 +5,38 @@ import rehypeRaw from "rehype-raw";
 function Previewer() {
   const [text, setText] = useState("");
   return (
-    <div className="container">
-      <div>
-        <textarea
-          name="text"
-          id=""
-          onChange={(e) => setText(e.target.value)}
-          cols="30"
-          rows="12"
-          placeholder="text"
-          title="Input Text"
-        ></textarea>
+    <>
+      <button
+        onClick={() =>
+          setText(
+            "# This is header\n## This is also header but smaller\nYou need to use space between codes"
+          )
+        }
+      >
+        ?
+      </button>
+      <div className="container">
+        <div>
+          <textarea
+            name="text"
+            id=""
+            onChange={(e) => setText(e.target.value)}
+            cols="30"
+            rows="12"
+            placeholder="text"
+            title="Input Text"
+            value={text}
+          ></textarea>
+        </div>
+        <div className="markdown">
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+            children={text}
+          ></ReactMarkdown>
+        </div>
       </div>
-      <div className="markdown">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeRaw]}
-          children={text}
-        ></ReactMarkdown>
-      </div>
-    </div>
+    </>
   );
 }
 
